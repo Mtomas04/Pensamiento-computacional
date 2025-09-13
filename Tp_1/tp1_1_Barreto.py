1a6c3e837d2bdd96a29146011cb749d595585c87
Tomas Barreto <mbarretoramirez@udesa.edu.ar>
2025-09-13 12:46:50 -0300

    Primer commit: se agrega la funcion y se termina el tp 1 la primer parte

--- Tp_1/tp1_1_Barreto.py
+++ Tp_1/tp1_1_Barreto.py
@@ -1,14 +1,14 @@
+from tp1_funciones_Barreto import siguiente_clima , climas
+import random  
 
-climas= ["Soleado",	"Nublado",	"Lluvioso",	"Tormenta"	,"Nevado"]
-
-tipo_clima=input("Ingrese estado inicial: ").capitalize()
+clima_actual=input("Ingrese estado inicial: ").capitalize()
 cantidad_dias= int(input("Ingrese cantidad de días: "))
 
-if tipo_clima not in climas :
+if clima_actual not in climas :
     print ("El estado inicial debe ser uno de: soleado, nublado, lluvioso, tormenta, nevado")
 elif cantidad_dias<0:
     print ("La cantidad de días debe ser un entero positivo")
-
-for i in climas:           ###Esto no va
-    c=2
-    
+else:
+    for dia in range(cantidad_dias + 1):
+        print(f"Día {dia}: {clima_actual}")
+        clima_actual = siguiente_clima(clima_actual)
--- Tp_1/tp1_funciones_Barreto.py
+++ Tp_1/tp1_funciones_Barreto.py
@@ -1,10 +1,18 @@
 import random
+climas= ["Soleado",	"Nublado",	"Lluvioso",	"Tormenta"	,"Nevado"]
+transiciones = {
+    "Soleado": {"Soleado": 0.60, "Nublado": 0.30, "Lluvioso": 0.05, "Tormenta": 0.03, "Nevado": 0.02},
+    "Nublado": {"Soleado": 0.40, "Nublado": 0.30, "Lluvioso": 0.20, "Tormenta": 0.05, "Nevado": 0.05},
+    "Lluvioso": {"Soleado": 0.10, "Nublado": 0.30, "Lluvioso": 0.40, "Tormenta": 0.15, "Nevado": 0.05},
+    "Tormenta": {"Soleado": 0.05, "Nublado": 0.10, "Lluvioso": 0.30, "Tormenta": 0.50, "Nevado": 0.05},
+    "Nevado":  {"Soleado": 0.05, "Nublado": 0.20, "Lluvioso": 0.20, "Tormenta": 0.10, "Nevado": 0.45}
+}
 
-def verificar_clima():
-    '''Esta funcion se encarga de guardar las listas de los climas y poner los dias'''
-    climas= ["Soleado",	"Nublado",	"Lluvioso",	"Tormenta"	,"Nevado"]
-    
-    return climas
+def siguiente_clima(actual):
+    opciones = list(transiciones[actual].keys())
+    probabilidades = list(transiciones[actual].values())
+    return random.choices(opciones, probabilidades)[0]
+
 
     
 
